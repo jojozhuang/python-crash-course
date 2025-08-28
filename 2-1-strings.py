799d075f65c425586279d728b84c429fe569072f
jojozhuang <jojozhuang@gmail.com>
2025-08-28 13:38:48 -0700

    add string examples

---
+++ 2-1-strings.py
@@ -0,0 +1,12 @@
+name = "hello WORLD"
+# Hello World
+print(name.title())
+
+# Hello world
+print(name.capitalize())
+
+# HELLO WORLD
+print(name.upper())
+
+# hello world
+print(name.lower())
---
+++ 2-3-tab-line.py
@@ -0,0 +1,21 @@
+# Python
+print("Python")
+
+#        Python (8 spaces before)
+print("\tPython")
+
+# Languages:
+# Python
+# C
+# JavaScript
+print("Languages:\nPython\nC\nJavaScript")
+
+favorite_language = ' python '
+#  python (without trailing space)
+print(favorite_language.rstrip())
+
+# python  (without leading space)
+print(favorite_language.lstrip())
+
+# python (without leading and trailing space)
+print(favorite_language.strip())
